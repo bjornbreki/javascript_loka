51972741b2627f6978aed84678698c61bfee8e7c
Björn Breki <bjbreki@gmail.com>
2017-05-03 15:29:55 +0000

    Stórt stökk í verkefninu.
    Núna er búið að bæta við plötum og alls 12 talsins. Hver með 300x300px og einnig til minni 150x150px. Plöturnar eru svo líka með sitt eigin array. Þar sem að það er farið yfir hverja plötu og hvað er það sem einkennir hana og hvernig hún er. Það kemur það til dæmis. Hvenær hún kom út, hvort þetta sé single eða album. Hvernig á að na í plötunna í gegnum / og rétt nöfn fyrir plöturnar.

---
+++ java_loka/script.js
@@ -0,0 +1,96 @@
+let listamenn = ["Beyoncé", "DJ_Snake", "Explosions_In_The_Sky", "MartinGarrix", "M83", "Halsey", "The_Weeknd", "The_Chainsmokers", "JackU", "Lorde", "Valdimar"];
+
+let Beyoncé{
+	"Plata":"BEYONCÉ",
+	"Standard":"img/standard/queen_b.png",
+	"Minni":"img/minni/queen_b.png",
+	"Form":"A",
+	"Date":"12/13/2013"
+};
+let DJ_Snake{
+	"Plata":"Encore",
+	"Standard":"img/standard/encore.jpg",
+	"Minni":"img/minni/encore.jpg",
+	"Form":"A",
+	"Date":"8/5/2016"
+};
+let Explosions_In_The_Sky{
+	"PLata":"Your Hand in Mine",
+	"Standard":"img/standard/your_hand_in_mine.jpg",
+	"Minni":"img/minni/your_hand_in_mine.jpg",
+	"Form":"A",
+	"Date":"11/4/2003"
+};
+let MartinGarrix{
+	"PLata":"Together",
+	"Standard":"img/standard/together.jpg",
+	"Minni":"img/minni/together.jpg",
+	"Form":"S",
+	"Date":"10/19/2016"
+};
+let M83{
+	"PLata":"Hurry Up We're Dreaming",
+	"Standard":"img/standard/HurryUpWereDreaming.jpg",
+	"Minni":"img/minni/HurryUpWereDreaming.jpg",
+	"Form":"A",
+	"Date":"10/18/2011"
+};
+let Halsey{
+	"Plata":"Colors",
+	"Standard":"img/standard/badlands.png",
+	"Minni":"img/minni/badlands.png"
+	"Form":"A",
+	"Date":"8/28/2015"
+};
+let Valdimar[
+{
+	"Plata":"Um Stund",
+	"Standard":"img/standard/um_stund.jpg",
+	"Minni":"img/minni/um_stund.jpg",
+	//"Sida":"um_stund.html",
+	"Form":"A",
+	"Date":"10/24/2012",
+},
+{
+	"Plata":"Batnar útsýnið",
+	"Standard":"img/standard/batnar.jpg",
+	"Minni":"img/minni/batnar.jpg",
+	"Form":"A",
+	"Date":"11/1/2014"
+}];
+let JackU{
+	"Plata":"Jack U",
+	"Standard":"img/standard/jack_u.jpg",
+	"Minni":"img/minni/jack_u.jpg",
+	"Form":"A",
+	"Date":"11/4/2016"
+};
+let Lorde{
+	"Plata":"Pure Herione",
+	"Standard":"img/standard/pure.png",
+	"Minni":"img/minni/pure.png",
+	"Form":"A",
+	"Date":"9/27/2013"
+};
+let The_Chainsmokers{
+	"Plata":"Collage",
+	"Standard":"img/standard/collage.jpg",
+	"Minni":"img/minni/collage.jpg",
+	"Form":"A",
+	"Date":""
+};
+let The_Weeknd{
+	"Plata":"Starboy",
+	"Standard":"img/standard/starboy.jpg",
+	"Minni":"img/minni/starboy.jpg",
+	"Form":"A",
+	"Date":"11/25/2016"
+};
+
+// let listamaður{
+//	"Nafnplötu"
+//	"Hver myndin af plötunni er og hvort hún er standard stærð."
+//	"Eða aðeins minni. Nánar tiltekið 150px af 300px."
+//	S er fyrir Single
+// 	A er fyrir Album
+//}
