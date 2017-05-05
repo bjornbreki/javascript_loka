6b0b38c6a5f5c15ad44f4dc5067426e240b39f91
Björn Breki <bjbreki@gmail.com>
2017-05-05 12:48:26 +0000

    Frammhald á javascript
    Núna er verið að fín pússa það sem er komið tengt breytum, nöfnum og tenginum.

--- java_loka/script.js
+++ java_loka/script.js
@@ -1,25 +1,40 @@
+
+document.write("Halló");
+let listamadur1 = document.getElementById('plata_nafn1');
+let listamadur2 = document.getElementById('plata_nafn2');
+let listamadur3 = document.getElementById('plata_nafn3');
+let listamadur4 = document.getElementById('plata_nafn4');
+let listamadur5 = document.getElementById('plata_nafn5');
+let listamadur6 = document.getElementById('plata_nafn6');
+let listamadur7 = document.getElementById('plata_nafn7');
+let listamadur8 = document.getElementById('plata_nafn8');
+let listamadur9 = document.getElementById('plata_nafn9');
+let listamadur10 = document.getElementById('plata_nafn10');
+let listamadur11 = document.getElementById('plata_nafn11');
+let listamadur12 = document.getElementById('plata_nafn12');
+
 let listamenn = ["Beyoncé", "DJ_Snake", "Explosions_In_The_Sky", "MartinGarrix", "M83", "Halsey", "The_Weeknd", "The_Chainsmokers", "JackU", "Lorde", "Valdimar"];
 
-let Beyoncé{
+let Beyoncé = {
 	"Plata":"BEYONCÉ",
 	"Standard":"img/standard/queen_b.png",
 	"Minni":"img/minni/queen_b.png",
 	"Form":"A",
 	"Date":"12/13/2013"
 };
-let DJ_Snake{
+let DJ_Snake = {
 	"Plata":"Encore",
 	"Standard":"img/standard/encore.jpg",
 	"Minni":"img/minni/encore.jpg",
 	"Form":"A",
-	"Date":"8/5/2016"
+	"Date":"08/05/2016"
 };
-let Explosions_In_The_Sky{
+let Explosions_In_The_Sky = {
 	"PLata":"Your Hand in Mine",
 	"Standard":"img/standard/your_hand_in_mine.jpg",
 	"Minni":"img/minni/your_hand_in_mine.jpg",
 	"Form":"A",
-	"Date":"11/4/2003"
+	"Date":"11/04/2003"
 };
 let MartinGarrix{
 	"PLata":"Together",
@@ -40,44 +55,43 @@ let Halsey{
 	"Standard":"img/standard/badlands.png",
 	"Minni":"img/minni/badlands.png"
 	"Form":"A",
-	"Date":"8/28/2015"
+	"Date":"08/28/2015"
 };
-let Valdimar[
+let Valdimar = [
 {
 	"Plata":"Um Stund",
 	"Standard":"img/standard/um_stund.jpg",
 	"Minni":"img/minni/um_stund.jpg",
-	//"Sida":"um_stund.html",
 	"Form":"A",
-	"Date":"10/24/2012",
+	"Date":"10/24/2012"
 },
 {
 	"Plata":"Batnar útsýnið",
 	"Standard":"img/standard/batnar.jpg",
 	"Minni":"img/minni/batnar.jpg",
 	"Form":"A",
-	"Date":"11/1/2014"
+	"Date":"11/01/2014"
 }];
 let JackU{
 	"Plata":"Jack U",
 	"Standard":"img/standard/jack_u.jpg",
 	"Minni":"img/minni/jack_u.jpg",
 	"Form":"A",
-	"Date":"11/4/2016"
+	"Date":"11/04/2016"
 };
 let Lorde{
 	"Plata":"Pure Herione",
 	"Standard":"img/standard/pure.png",
 	"Minni":"img/minni/pure.png",
 	"Form":"A",
-	"Date":"9/27/2013"
+	"Date":"09/27/2013"
 };
 let The_Chainsmokers{
 	"Plata":"Collage",
 	"Standard":"img/standard/collage.jpg",
 	"Minni":"img/minni/collage.jpg",
 	"Form":"A",
-	"Date":""
+	"Date":"11/04/2016"
 };
 let The_Weeknd{
 	"Plata":"Starboy",
@@ -94,3 +108,40 @@ let The_Weeknd{
 //	S er fyrir Single
 // 	A er fyrir Album
 //}
+
+let __1 = Valdimar[0].Plata;
+listamadur1.textContent = __1;
+
+let __2 = Explosions_In_The_Sky.PLata;
+listamadur2.textContent = __2;
+
+let __3 = M83.PLata;
+listamadur3.textContent = __3;
+
+let __4 = Lorde.Plata;
+listamadur4.textContent = __4;
+
+let __5 = Beyoncé.Plata;
+listamdur5.textContent = __5;
+
+let __6 = Valdimar[1].PLata;
+listamadur6.textContent = __6;
+
+let __7 = DJ_Snake.Plata;
+listamadur7.textContent = __7;
+
+let __8 = The_Chainsmokers.Plata;
+listamadur8.textContent = __8;
+
+let __9 = The_Weeknd.Plata;
+listamadur9.textContent = __9;
+
+let __10 = Halsey.Plata;
+listamadur10.textContent = __10;
+
+let __11 = JackU.Plata;
+listamadur11.textContent = __11;
+
+let __12 = MartinGarrix.Plata;
+listamadur12.textContent = __12;
+
